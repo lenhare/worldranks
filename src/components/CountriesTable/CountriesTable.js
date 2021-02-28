c955a5457819083cc439e36c6646c781754d76c4
Renan Lenhare <lenhare@outlook.com>
2021-02-28 13:55:04 -0300

    Order Filter

--- src/components/CountriesTable/CountriesTable.js
+++ src/components/CountriesTable/CountriesTable.js
@@ -1,34 +1,88 @@
-import { StylesProvider } from '@material-ui/core';
-import { KeyboardArrowDownRounded } from '@material-ui/icons';
-import styles from './CountriesTable.module.css'
+import { StylesProvider } from "@material-ui/core";
+import {
+  KeyboardArrowDownRounded,
+  KeyboardArrowUpRounded,
+} from "@material-ui/icons";
+import { useState } from "react";
+import styles from "./CountriesTable.module.css";
 
-const orderBy = (countries, direction) => {
-    if(direction === 'asc'){
-        return [...countries].sort((a,b) => (a.population > b.population ? 1: -1));
+const orderBy = (countries, value, direction) => {
+  if (direction === "asc") {
+    return [...countries].sort((a, b) => (a[value] > b[value] ? 1 : -1));
+  }
+  if (direction === "desc") {
+    return [...countries].sort((a, b) => (a[value] > b[value] ? -1 : 1));
+  }
+  return countries;
+};
+
+const SortArrow = ({ direction }) => {
+  if (!direction) {
+    return <> </>;
+  }
+
+  if (direction === "desc") {
+    return (
+      <div className={styles.heading_arrow}>
+        <KeyboardArrowDownRounded color="inherit" />
+      </div>
+    );
+  } else {
+    return (
+      <div className={styles.heading_arrow}>
+        <KeyboardArrowUpRounded color="inherit" />
+      </div>
+    );
+  }
+};
+const CountriesTable = ({ countries }) => {
+  const [direction, setDirection] = useState();
+  const [value, setValue] = useState();
+
+  const orderedCountries = orderBy(countries, value, direction);
+
+  const switchDirection = () => {
+    if (!direction) {
+      setDirection("desc");
+    } else if (direction === "desc") {
+      setDirection("asc");
+    } else {
+      setDirection(null);
     }
-    if(direction === 'desc'){
-        return [...countries].sort((a,b) => (a.population > b.population ? -1: 1));
-    }
-    return countries;
-}
-const CountriesTable = ({countries}) => {
-    const orderedCountries = orderBy(countries,"desc" );
-    return(
-        <div>
-            <div className={styles.heading}>
-                <button className={styles.heading_name}><div>Nome</div>
-                <KeyboardArrowDownRounded />
-                </button>
-                <button className={styles.heading_population}><div>População</div>
-                <KeyboardArrowDownRounded />
-                </button>
-            </div>
-            {orderedCountries.map((country)=>(<div className={styles.row}>
-                <div className={styles.name}>{country.name}</div>
-                <div className={styles.population}>{country.population}</div>
-            </div>))}
+  };
+
+  const setValueAndDirection = (value) => {
+    switchDirection();
+    setValue(value);
+  };
+  return (
+    <div>
+      <div className={styles.heading}>
+        <button
+          className={styles.heading_name}
+          onClick={() => setValueAndDirection("name")}
+        >
+          <div>Nome</div>
+
+          <SortArrow direction={direction} />
+        </button>
+        <button
+          className={styles.heading_population}
+          onClick={() => setValueAndDirection("population")}
+        >
+          <div>População</div>
+
+          <SortArrow direction={direction} />
+        </button>
+      </div>
+      {orderedCountries.map((country) => (
+        <div className={styles.row}>
+          <div className={styles.name}>{country.name}</div>
+          <div className={styles.population}>{country.population}</div>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
 export default CountriesTable;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,23 +1,20 @@
-import Head from 'next/head'
-import styles from './Layout.module.css'
+import Head from "next/head";
+import styles from "./Layout.module.css";
 
-const Layout = ({children, title="World Ranks"}) => {
-    return(
+const Layout = ({ children, title = "World Ranks" }) => {
+  return (
     <div className={styles.container}>
-    <Head>
-      <title>{title}</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <header className={styles.header} >
-        <img src="Logo.svg" alt="Logo"/>
-    </header>
-    <main className={styles.main}>
-      {children}
-    </main>
-    <footer className={styles.footer}>
-     Renan Lenhare
-    </footer>
-  </div>);
-}
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className={styles.header}>
+        <img src="Logo.svg" alt="Logo" />
+      </header>
+      <main className={styles.main}>{children}</main>
+      <footer className={styles.footer}>Renan Lenhare</footer>
+    </div>
+  );
+};
 
 export default Layout;
